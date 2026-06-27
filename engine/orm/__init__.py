8b348113bb8088bbe8122e0fdf2a36431ae10815
Taha Akbarally <taha.akbarally@gmail.com>
2026-06-27 20:12:06 +1000

    clean half

--- engine/orm/__init__.py
+++
@@ -1,4 +0,0 @@
-from game.orm.token_definition import GameBase, TokenDefinition
-from game.orm.game_snapshot import GameSnapshot
-
-__all__ = ["GameBase", "TokenDefinition", "GameSnapshot"]
--- engine/tools/positions.py
+++
@@ -1,9 +0,0 @@
-from typing import NamedTuple
-from engine.tools import positions
-
-class Position(NamedTuple):
-    x: int
-    y: int
-
-    def displacement(self, target: "Position") -> "Position":
-        return Position(x=target.x-self.x, y=target.y-self.y)
